f1190f2bfb977c89ea11a2cd93b66467a822fed0
Truong Vinh Tran <truongvinht@gmail.com>
2022-12-29 19:08:14 +0100

    integrate ag grid sample
    integrate ag grid sample

---
+++ app/genshin/elements/page.jsx
@@ -0,0 +1,32 @@
+'use client';
+import { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+
+const ElementsOverview = () => {
+  const pulls = [{ element:'Pyro',object_ref: "demo", order_index: 'i01'}];
+
+  const [columnDefs] = useState([
+    { headerName: 'Element', field: 'element' },
+    { headerName: 'Ref', field: 'object_ref' },
+    { headerName: 'Index', field: 'order_index' }
+  ]);
+
+  return (
+    <div>
+      <h1 className="text-xl md:text-5xl text-center font-bold py-10">
+        Elemente
+      </h1>
+
+      <div className="ag-theme-alpine" style={{ height: "80%", width: "100%" }}>
+        <AgGridReact
+        id="staff_grid"
+        rowData={pulls}
+        columnDefs={columnDefs}
+        style={{ height: '100%', width: '100%' }}
+      ></AgGridReact>
+      </div>
+    </div>
+  );
+};
+
+export default ElementsOverview;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 import Navigator from '../components/navigator'
 
@@ -6,7 +8,7 @@ export default function RootLayout({ children }) {
   const title = 'Gaming'; 
   return (
     <html>
-      <head appTitle={title}/>
+      <head />
       <body>
         <Navigator appTitle={title}/>
         {children}
