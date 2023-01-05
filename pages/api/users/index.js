501ed4e6ebdb993e842cfff78bb9cccab2096899
Truong Vinh Tran <truongvinht@gmail.com>
2023-01-05 11:14:53 +0100

    prevent creating user without password via api

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,6 +1,6 @@
 import { findAllHandler } from "../../../utils/mongoHandler";
 import Model from "../../../models/User";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 export default async function handler(req, res) {
   const { method } = req;
@@ -15,15 +15,17 @@ export default async function handler(req, res) {
       res.status(200).json({ success: false, message: "already registered" });
       return;
     }
-    console.log(JSON.stringify(body));
+
+    if (req.body.password == null || req.body.password === undefined) {
+      res.status(400).json({ success: false, message: "invalid password" });
+      return;
+    }
 
     const salt = await bcrypt.genSalt(10);
     const hashPass = await bcrypt.hash(req.body.password, salt);
     body.password = hashPass;
     try {
-      const obj = await Model.create(
-        body
-      ); 
+      const obj = await Model.create(body);
       res.status(201).json({ success: true, data: obj });
     } catch (error) {
       console.log(error);
