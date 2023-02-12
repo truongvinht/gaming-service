d7839cd9c6bf6247fe7611034c87b4b8e1b0218e
Truong Vinh Tran <truongvinht@gmail.com>
2023-02-12 17:37:42 +0100

    add counter for figures

--- app/genshin/characters/CharacterOverviewPage.jsx
+++ app/genshin/characters/CharacterOverviewPage.jsx
@@ -35,7 +35,7 @@ const CharacterOverviewPage = () => {
   return (
     <div>
       <h1 className="text-xl md:text-5xl text-center font-bold py-10">
-        Figuren
+        Figuren ({rows.length})
       </h1>
       <CardCollectionGrid
         cards={rows.map((obj) => (
